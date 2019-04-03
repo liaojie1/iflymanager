607b9587211aa60ca54bdbffeb17c20f89d5efbb
liaojie <827763583@qq.com>
2019-04-03 18:06:48 +0800

    firstgit

--- src/page/demo/Life.js
+++ src/page/demo/Life.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Child from './child';
 import "./index.less"
+import { Button } from "antd"
 class Life extends Component {
     constructor(props) {
         super(props);
@@ -16,7 +17,7 @@ class Life extends Component {
     }
     hanleClick() {
         this.setState({
-            count: this.state.count + 1
+            count: this.state.count - 1
         });
     }
 
@@ -27,6 +28,7 @@ class Life extends Component {
         return (
             <div className="content">
                 <p>React生命周期试用</p>
+                <Button onClick={this.handeleAdd}>Antd点击一下</Button>
                 <button onClick={this.handeleAdd}>点击一下</button>
                 <button onClick={this.hanleClick.bind(this)} > 点击一下</button>
                 <p>{this.state.count}</p>
